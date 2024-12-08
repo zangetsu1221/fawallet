b7b24ef88e0966c00ab22007121be6d74d3c7df7
zangetsu1221 <142219015+zangetsu1221@users.noreply.github.com>
2024-12-08 02:27:05 +0000

    Dummy files
    an upload of the dummy project for other members to work on

---
+++ lib/flutter_flow/custom_icons.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+
+class FFIcons {
+  FFIcons._();
+
+  static const String _icomoonFamily = 'Icomoon';
+
+  // icomoon
+  static const IconData kcardFill =
+      IconData(0xe901, fontFamily: _icomoonFamily);
+  static const IconData kcard = IconData(0xe902, fontFamily: _icomoonFamily);
+  static const IconData khomeFill =
+      IconData(0xe903, fontFamily: _icomoonFamily);
+  static const IconData khome = IconData(0xe904, fontFamily: _icomoonFamily);
+  static const IconData knotification =
+      IconData(0xe905, fontFamily: _icomoonFamily);
+  static const IconData kprofileFill =
+      IconData(0xe906, fontFamily: _icomoonFamily);
+  static const IconData kprofile = IconData(0xe907, fontFamily: _icomoonFamily);
+  static const IconData ksendMoney =
+      IconData(0xe908, fontFamily: _icomoonFamily);
+  static const IconData kstatsFill =
+      IconData(0xe909, fontFamily: _icomoonFamily);
+  static const IconData kstats = IconData(0xe90a, fontFamily: _icomoonFamily);
+  static const IconData ktopUp = IconData(0xe90b, fontFamily: _icomoonFamily);
+  static const IconData ktransactionFill =
+      IconData(0xe900, fontFamily: _icomoonFamily);
+  static const IconData ktransaction =
+      IconData(0xe90c, fontFamily: _icomoonFamily);
+  static const IconData kwithdraw =
+      IconData(0xe90d, fontFamily: _icomoonFamily);
+}
